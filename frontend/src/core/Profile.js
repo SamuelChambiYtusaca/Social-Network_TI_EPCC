fae94cae9aee9110b828dc16f0f6d78625025718
Juan Carlos Postigo <jpostigo@unsa.edu.pe>
2021-06-24 14:57:18 -0500

    Corrección 002

--- frontend/src/core/Profile.js
+++ frontend/src/core/Profile.js
@@ -2,7 +2,7 @@ import React, { useState }  from "react";
 import Body from "../components/signIn";
 import { signin, authenticate, isAuthenticated } from './apiCore';
 import { Alert, Row, Col, Nav } from 'reactstrap';
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import "./styles/Login.css";
 import NavBar from "../components/nav-bar";
 import SearchBar from "../components/search-bar";
@@ -14,6 +14,9 @@ const Profile = (req, res) => {
       
       <SearchBar/>
       <NavBar/>
+      <Link to="/logout" className="alert-link">
+        Salir
+      </Link>
     </div>
   );
 };
